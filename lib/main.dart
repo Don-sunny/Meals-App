8a82968816d1465ffd61b91ee5aea89a14763698
Don-sunny <donsunny544@gmail.com>
2023-07-09 18:18:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+    useMaterial3: true,
+    colorScheme:
+        ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 131, 57, 0)),
+    textTheme: GoogleFonts.latoTextTheme());
+
+void mian() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+      theme: theme,
+    );
+  }
+}
